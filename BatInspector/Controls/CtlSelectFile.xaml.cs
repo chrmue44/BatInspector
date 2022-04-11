1ad79209fd433bafb88c22709f39fa2c9f66a1ec
Christian Müller <chrmue44@gmail.com>
2022-04-11 13:45:59 +0200

    Localization, global report

---
+++ BatInspector/Controls/CtlSelectFile.xaml.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.Win32;
+using System.Windows;
+using System.Windows.Controls;
+//using System.Windows.Forms;
+
+namespace BatInspector.Controls
+{
+  /// <summary>
+  /// Interaktionslogik für CtlSelectFile.xaml
+  /// </summary>
+  public partial class CtlSelectFile : UserControl
+  {
+    public CtlSelectFile()
+    {
+      InitializeComponent();
+      IsFolder = false;
+    }
+
+    public bool IsFolder { get; set; }
+    private void _btnOpen_Click(object sender, RoutedEventArgs e)
+    {
+      if (IsFolder)
+      {
+        System.Windows.Forms.FolderBrowserDialog ofo = new System.Windows.Forms.FolderBrowserDialog();
+        System.Windows.Forms.DialogResult res = ofo.ShowDialog();
+        if (res == System.Windows.Forms.DialogResult.OK)
+        {
+          _txt.Text = ofo.SelectedPath;
+        }
+      }
+      else
+      {
+        OpenFileDialog ofi = new OpenFileDialog();
+      }
+    }
+  }
+}
